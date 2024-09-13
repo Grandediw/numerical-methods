533e3df9a16d6477b0d0173e00c0bc3936046195
Stefano Tonini <72923946+Grandediw@users.noreply.github.com>
2024-09-13 15:19:53 -0700

    Add files via upload

---
+++ gianperde/gianperde/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  gianperde
+//
+//  Created by Stefano Tonini on 25/02/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n=3;
+    for(int i=n-1;i>=0;i--){
+        cout <<i<<endl;
+    }
+    return 0;
+}
